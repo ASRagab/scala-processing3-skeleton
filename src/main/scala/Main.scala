63079d5bed0a7d4d869cc6018ab3a8530fc758aa
Ahmad Ragab <Averroes2006@gmail.com>
2016-03-08 18:56:59 -0500

    kicking off multiple sketches works less well from sbt, but it works

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,12 +17,12 @@ class Main extends PApplet {
       saveFrame("stills/output-####.tga")
   }
 
-  override def settings = {
-    size(600, 800);
+  override def settings() = {
+    size(600, 800)
   }
 
   def renderer(initial: Float) = {
-    noFill
+    noFill()
     (1 to 50).foreach { i =>
       stroke(i, i * 2, 50 + i, 2)
       ellipse(width / 2.0f, height / 2.0f, i * initial, i * 20 * noise(10 - initial))
@@ -30,9 +30,40 @@ class Main extends PApplet {
   }
 }
 
-object Main extends PApplet {
+class Other extends PApplet {
+  private val saveFrames: Boolean = false;
+
+  override def setup() = {
+
+    background(255)
+    stroke(200, 0, 100, 2)
+    rectMode(CORNERS)
+  }
+
+  override def draw() = {
+    renderer(frameCount / 100.0f)
+    if(saveFrames)
+      saveFrame("stills/output-####.tga")
+  }
+
+  override def settings() = {
+    size(600, 800)
+  }
+
+  def renderer(initial: Float) = {
+    noFill()
+    (1 to 50).foreach { i =>
+      stroke(i*5, i, 50 + i, 1)
+      ellipse(width / 2.0f, height / 2.0f, i * initial, i * 20 * noise(10 - initial))
+    }
+  }
+}
+
+object Runner extends PApplet {
   def main(args: Array[String]) = {
     val test = new Main()
+    val other = new Other()
     PApplet.runSketch(Array("Main"), test)
+    PApplet.runSketch(Array("Other"), other)
   }
 }
