bf0e823341d08cc51efe6e90c5979bcc6938e0ad
Ahmad Ragab <Averroes2006@gmail.com>
2016-07-30 00:58:16 -0400

    update Processing Version

--- build.sbt
+++ build.sbt
@@ -5,10 +5,10 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq (
-  "org.processing" % "core" % "3.0.1",
-  "org.processing" % "net" % "3.0.1",
-  "org.processing" % "video" % "3.0.1",
-  "org.processing" % "serial" % "3.0.1",
-  "org.processing" % "pdf" % "3.0.1"
+  "org.processing" % "core" % "3.1.1",
+  "org.processing" % "net" % "3.1.1",
+  "org.processing" % "video" % "3.0.2",
+  "org.processing" % "serial" % "3.1.1",
+  "org.processing" % "pdf" % "3.1.1"
 )
     
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import processing.core.PConstants._
 import processing.core._
 
 class Main extends PApplet {
-  private val saveFrames: Boolean = false;
+  private val saveFrames: Boolean = true
 
   override def setup() = {
 
@@ -13,12 +13,12 @@ class Main extends PApplet {
 
   override def draw() = {
     renderer(frameCount / 100.0f)
-    if(saveFrames)
+    if (saveFrames && frameCount % 1000 == 0)
       saveFrame("stills/output-####.tga")
   }
 
   override def settings() = {
-    size(600, 800)
+    size(1600, 900)
   }
 
   def renderer(initial: Float) = {
@@ -31,30 +31,38 @@ class Main extends PApplet {
 }
 
 class Other extends PApplet {
-  private val saveFrames: Boolean = false;
+  private var saveFrames: Boolean = false
 
   override def setup() = {
 
     background(255)
     stroke(200, 0, 100, 2)
-    rectMode(CORNERS)
+    beginRecord(PDF, "output-####.pdf")
+
   }
+  
+  override def mousePressed() = saveFrames = true
 
   override def draw() = {
-    renderer(frameCount / 100.0f)
-    if(saveFrames)
-      saveFrame("stills/output-####.tga")
+  
+    renderer(frameCount / 200.0f)
+  
+    if (saveFrames) {
+      endRecord()
+      saveFrames = false
+    }
   }
 
   override def settings() = {
-    size(600, 800)
+    size(1600, 900)
+    smooth(8)
   }
 
   def renderer(initial: Float) = {
     noFill()
     (1 to 50).foreach { i =>
-      stroke(i*5, i, 50 + i, 1)
-      ellipse(width / 2.0f, height / 2.0f, i * initial, i * 20 * noise(10 - initial))
+      stroke(i * 5, i * i, i, 1)
+      ellipse(width / 2.0f, height / 2.0f, i * 10 * initial * noise(i), i * 20 * noise(i % initial))
     }
   }
 }
@@ -63,7 +71,8 @@ object Runner extends PApplet {
   def main(args: Array[String]) = {
     val test = new Main()
     val other = new Other()
-    PApplet.runSketch(Array("Main"), test)
-    PApplet.runSketch(Array("Other"), other)
+    PApplet.runSketch(Array("test"), test)
+
   }
+  
 }
